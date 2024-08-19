6288f65c5aaa9d6f64128bdef720c556396c805e
MuhammetYusufBULUT <muhammet226179@ogr.duzce.edu.tr>
2024-08-19 17:13:22 +0300

    Add Autofac support to Car Rental project
    - Integrated Autofac as the dependency injection container.
    - Configured dependency registration for core services and data access layers.
    - Updated the project to use Autofac for better modularity and testability.

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstarct;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Burada hangi interface'in karşılığı ne olacak onu belirtiyoruz
+            
+       
+            builder.RegisterType<CarManager>().As<ICarService>().InstancePerLifetimeScope();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerManager>().As<ICustomerService>().InstancePerLifetimeScope();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().InstancePerLifetimeScope();
+            builder.RegisterType<RentalManager>().As<IRentalService>().InstancePerLifetimeScope();
+            builder.RegisterType<EfRentalDal>().As<IRentalDal>().InstancePerLifetimeScope();
+            builder.RegisterType<UserManager>().As<IUserService>().InstancePerLifetimeScope();
+            builder.RegisterType<EfUserDal>().As<IUserDal>().InstancePerLifetimeScope();
+
+          
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,51 +1,31 @@
-using Business.Abstarct;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 namespace WebAPI
 {
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            
-            builder.Services.AddScoped<ICarService, CarManager>();
-            builder.Services.AddScoped<ICarDal, EfCarDal>();
-            builder.Services.AddScoped<ICustomerService, CustomerManager>();
-            builder.Services.AddScoped<ICustomerDal, EfCustomerDal>();
-            builder.Services.AddScoped<IRentalService, RentalManager>();
-            builder.Services.AddScoped<IRentalDal, EfRentalDal>();
-            builder.Services.AddScoped<IUserService, UserManager>();
-            builder.Services.AddScoped<IUserDal, EfUserDal>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Products}/{action=Get}/{id?}");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory()) //===> bunu ekleyerek Autofac kullanmam?z? sa?lar
+                .ConfigureContainer<ContainerBuilder>(builder =>
+                {
+                    builder.RegisterModule(new AutofacBusinessModule());
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
